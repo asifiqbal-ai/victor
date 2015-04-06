4d0e4c77cb5b410e5fe05c69308677826b357137
Richard Agyei <richard.agyei@thomsonreuters.com>
2015-04-06 16:17:04 -0500

    committing initial website changes

---
+++ app/routes/main-routes.js
@@ -0,0 +1,42 @@
+// app/routes/main-routes.js
+module.exports = function (router) {
+
+    // server routes ===========================================================
+    // handle things like api calls
+    // authentication routes
+
+    // sample api route
+    router.get('/about', function (req, res) {
+        // use mongoose to get all nerds in the database
+       /* Nerd.find(function (err, nerds) {
+
+            // if there is an error retrieving, send the error. 
+            // nothing after res.send(err) will execute
+            if (err)
+                res.send(err);
+
+            res.json(nerds); // return all nerds in JSON format
+        });*/
+        res.render('about')
+    });
+
+    // route to handle creating goes here (app.post)
+    // route to handle delete goes here (app.delete)
+    
+    router.get('/contact', function(req, res){
+       //res.render('contact'); 
+       res.render('contact', {layout: 'contacts-layout' });
+    });
+
+     router.get('/', function(req, res){
+       res.render('index'); 
+    });
+
+    // frontend routes =========================================================
+    // route to handle all angular requests
+    router.get('*', function (req, res) {
+        //res.sendfile('./public/views/index.html'); // load our public/index.html file
+        res.send("404");
+    });
+
+};
---
+++ server.js
@@ -0,0 +1,59 @@
+// server.js
+
+// modules =================================================
+//require and use express
+var express = require('express'),
+    app = express();
+
+//create the router to be used to create links
+var router = express.Router();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main.hbs',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+// configuration ===========================================
+
+// config files
+var db = require('./config/db');
+// set our port
+var port = process.env.PORT || 3000;
+
+// get all data/stuff of the body (POST) parameters
+// parse application/json 
+app.use(bodyParser.json());
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+}));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+
+// routes ==================================================
+require('./app/routes/main-routes')(router); // configure our routes
+
+//use the router
+app.use('/', router);
+
+// START THE SERVER
+// ==============================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+// expose app           
+exports = module.exports = app;
