dc0ee21a5e45eb625e705c34b0acb20d1292de11
rnkoaa <richard.agyei@gmail.com>
2015-04-20 00:25:11 -0500

    Get all data from forms

--- public/assets/js/angular/controllers/registration.js
+++ public/assets/js/angular/controllers/registration.js
@@ -5,10 +5,38 @@ angular.module('victorApp.controllers', [])
     .controller('registerController', ['$scope', 'registerFactory',
         function ($scope, registerFactory) {
             $scope.registerForm = {};
-            $scope.playerGenders = [ 'Male', 'Female', 'Other'];
+            $scope.registerForm.parent = {};
+            $scope.registerForm.player = {};
+            $scope.registerForm.alternate = {};
+            $scope.registerForm.player.birthDay = {};
+
+            $scope.playerGenders = [
+                {'id': 'gender', 'name': 'Gender'}, {
+                    'id': 'male',
+                    'name': 'Male'
+                }, {'id': 'female', 'name': 'Female'},
+                {'id': 'other', 'name': 'Other'}];
+            $scope.birthMonths = [
+                {'id': 'january', 'name': 'January'},
+                {'id': 'february', 'name': 'February'},
+                {'id': 'march', 'name': 'March'},
+                {'id': 'april', 'name': 'January'},
+                {'id': 'may', 'name': 'May'},
+                {'id': 'june', 'name': 'June'},
+                {'id': 'july', 'name': 'July'},
+                {'id': 'august', 'name': 'August'},
+                {'id': 'september', 'name': 'September'},
+                {'id': 'october', 'name': 'October'},
+                {'id': 'november', 'name': 'November'},
+                {'id': 'december', 'name': 'December'}
+            ];
+            $scope.selectedMonth = $scope.birthMonths[0];
+            $scope.selectedGender = $scope.playerGenders[0];
             //$scope.registerForm.playerGender = $scope.playerGenders[0];
 
             $scope.processForm = function () {
+                $scope.registerForm.player.birthDay.month = $scope.selectedMonth;
+                $scope.registerForm.gender = $scope.selectedGender;
                 console.log($scope.registerForm)
             };
             /*$scope.reqContact = {};
